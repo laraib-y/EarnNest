915b752de7f923da65a113b810a6362bf9a06f81
Laraib <laraib.yousfani@gmail.com>
2026-03-07 19:13:38 -0800

    style: updated logo w cute crow :)

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
         <div className={styles.mascot}>
           <Image
-            src="/mascot.jpeg"
+            src="/assets/MascotLogo.svg"
             alt="EarnNest Mascot"
             fill
             priority
